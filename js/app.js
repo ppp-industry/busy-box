3179da3dac02f3bbac03a07ad7a72d64f2ef3e63
Petro Pasteruk <petropasteruk@gmail.com>
2022-09-05 06:38:55 +0300

    Complete all

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ function accordion() {
 
 accordion();
 
-function modal () {
+function modal() {
 
     document.addEventListener('click', modalHandler);
 
@@ -69,7 +69,7 @@ function modal () {
 
 }
 
-modal ()
+modal()
 
 const swiperProductThumbs = new Swiper('.product-swiper-thumbs', {
     spaceBetween: 3,
@@ -93,4 +93,74 @@ const swiperProduct = new Swiper('.product-swiper', {
 const gallery = new Viewer(document.getElementById('gallery-items'), {
     navbar: 0,
     title: false,
+    movable: false,
+    // rotatable: false,
+    // scalable: false
 });
+
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+    document.querySelectorAll(".parallax-effect").forEach(function (move) {
+        const moving_value = move.getAttribute("data-value");
+
+        const x = (e.clientX * moving_value) / 250;
+        const y = (e.clientY * moving_value) / 250;
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    });
+}
+
+(function() {
+    let timers = document.querySelectorAll(".timer");
+    timers.forEach(timer => {
+        let countHours, countMinutes, countSeconds, h, m, s;
+        countHours = null;
+        countMinutes = null;
+        countSeconds = null;
+
+        h = timer.querySelector(".hours");
+        m = timer.querySelector(".minutes");
+        s = timer.querySelector(".seconds");
+
+        setInterval(function() {
+            let date, nowHours, nowMinutes, nowSeconds;
+            date = new Date();
+            nowHours = date.getHours();
+            nowMinutes = date.getMinutes();
+            nowSeconds = date.getSeconds();
+            if (countSeconds !== nowSeconds) {
+                s.classList.add("pop");
+                setTimeout(function() {
+                    return s.classList.remove("pop");
+                }, 300);
+                countSeconds = 60 - nowSeconds;
+                if (nowSeconds === 0) {
+                    countSeconds = 0;
+                }
+                s.textContent = (countSeconds < 10 ? "0" : "") + countSeconds;
+            }
+            if (countMinutes !== nowMinutes && nowSeconds !== 0 || countMinutes === null) {
+                m.classList.add("pop");
+                setTimeout(function() {
+                    return m.classList.remove("pop");
+                }, 300);
+                countMinutes = 59 - nowMinutes;
+                if (nowMinutes === 0) {
+                    countMinutes = 59;
+                }
+                m.textContent = (countMinutes < 10 ? "0" : "") + countMinutes;
+                countMinutes = nowMinutes;
+            }
+            if (countHours !== nowHours && nowMinutes === 0 && nowSeconds === 1 || countHours === null) {
+                h.classList.add("pop");
+                setTimeout(function() {
+                    return h.classList.remove("pop");
+                }, 300);
+                countHours = 23 - nowHours;
+                h.textContent = (countHours < 10 ? "0" : "") + countHours;
+                return countHours = nowHours;
+            }
+        }, 1000);
+    });
+}).call(this);
